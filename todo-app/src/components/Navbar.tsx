cd7bde28373ebd00d98c29f25c31cdaac3aa2041
Shardendu Mishra <shardendumishrabrave@gmail.com>
2024-10-29 03:06:14 +0530

    Initialising

--- todo-app/src/components/Navbar.tsx
+++ todo-app/src/components/Navbar.tsx
@@ -3,7 +3,17 @@
 const Navbar = () => {
   return (
     <div>
-      <h1>Navbar</h1>
+      <nav>
+        <a href="https://www.linkedin.com/in/shardendumishra22/" target="_blank" rel="noopener noreferrer">
+          LinkedIn
+        </a>
+        <h1>
+          Todo App
+        </h1>
+        <a href="https://github.com/MishraShardendu22" target="_blank" rel="noopener noreferrer">
+          Github
+        </a>
+      </nav>
     </div>
   )
 }
